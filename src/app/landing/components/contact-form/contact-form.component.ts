33fff256ed14a8009019d2997d3cc78a1987a165
Ivan <ivan.mondragon.sotres@gmail.com>
2023-12-28 23:05:31 -0600

    (ADD) Added reactive form to manage contact form

--- src/app/landing/components/contact-form/contact-form.component.ts
+++ src/app/landing/components/contact-form/contact-form.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Contact } from '../../models/contact.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+  styleUrls: ['./contact-form.component.scss'],
 })
 export class ContactFormComponent {
+  public contactForm = new FormGroup({
+    email: new FormControl<string>('', [
+      Validators.required,
+      Validators.maxLength(250),
+    ]),
+    name: new FormControl<string>('', [
+      Validators.required,
+      Validators.maxLength(250),
+    ]),
+    message: new FormControl<string>('', [
+      Validators.required,
+      Validators.maxLength(250),
+    ]),
+  });
 
+  get newContact(): Contact {
+    const item = this.contactForm.value as Contact;
+    return item;
+  }
+  public onSubmit(): void {
+    if (this.contactForm.invalid) return;
+    console.log(this.newContact);
+  }
 }
